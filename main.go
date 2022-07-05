cd016d2a1316c7ea856aee9291ebef6fa039c087
Lyly <khlbolatovna@gmail.com>
2022-07-05 20:31:16 +0600

    fixed all errors

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 )
 
 var (
@@ -17,79 +18,273 @@ var (
 	filename              = flag.String("fileName", "", "usage")
 	errorFlagFile         = "-fileName flag has to be specified"
 	OperationMissingError = "-operation flag has to be specified"
+	errFlagItem           = "-item flag has to be specified"
 	errNoSuchFile         = "open : no such file or directory"
+	errSameId             = "Item with id 1 already exists"
+	errIdFlagMissing      = "-id flag has to be specified"
 )
 
 type Arguments map[string]string
 
-func Add(args Arguments) error {
+type Item struct {
+	Id    string `json:"id"`
+	Email string `json:"email"`
+	Age   int    `json:"age"`
+}
+
+func Add(args Arguments, writer io.Writer) error {
 	if len(args["item"]) == 0 {
-		return errors.New("")
+		return errors.New(errFlagItem)
 	}
-	err := Openfile(args)
+
+	err := CheckIsFileOpen(args)
 	if err != nil {
 		return err
 	}
 
-	result, err := MarshalData(args)
-	ioutil.WriteFile(args["fileName"], result, 0644)
+	err = CheckItemID(args, writer)
+	if err != nil {
+		err = WriteOut([]byte(errSameId), writer)
+		if err != nil {
+			return err
+		}
+	}
+
+	str := "[" + args["item"] + "]"
+
+	err = ioutil.WriteFile(args["fileName"], []byte(str), 0644)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
-func List(name string, writer io.Writer) error {
-	bytes, err := ioutil.ReadFile(name)
+func List(args Arguments, writer io.Writer) ([]byte, error) {
+	bytes, err := Readfile(args["fileName"], writer)
 	if err != nil {
-		return err
+		return nil, err
 	}
+	err = WriteOut(bytes, writer)
+	if err != nil {
+		return nil, err
+	}
+
 	if len(bytes) == 0 {
-		return nil
+		return nil, nil
 	}
+	return bytes, nil
+}
 
-	writer.Write(bytes)
+func FindByID(args Arguments, writer io.Writer) error {
+	if args["id"] == "" {
+		return errors.New(errIdFlagMissing)
+	} else {
+		jsonData, err := Readfile(args["fileName"], writer)
+		if err != nil {
+			return err
+		}
 
+		unmarshaledItem, err := UnmarshalData(jsonData)
+		if err != nil {
+			log.Println("err", err)
+			return err
+
+		}
+
+		for _, value := range unmarshaledItem {
+			if value.Id == args["id"] {
+				jsonData, err := MarshalData(value)
+				if err != nil {
+					return err
+				}
+				err = WriteOut(jsonData, writer)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
 	return nil
 }
 
-func FindByID() error {
+func Remove(args Arguments, writer io.Writer) error {
+	var item []Item
+
+	if args["id"] == "" {
+		return errors.New(errIdFlagMissing)
+	} else {
+		jsonData, err := Readfile(args["fileName"], writer)
+		if err != nil {
+			return err
+		}
+
+		unmarshaledItem, err := UnmarshalData(jsonData)
+		if err != nil {
+			return err
+		}
+
+		exist := IsIDExist(unmarshaledItem, args)
+
+		if !exist {
+			err = WriteOut([]byte("Item with id "+args["id"]+" not found"), writer)
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+
+		for _, value := range unmarshaledItem {
+			if value.Id != args["id"] {
+				item = append(item, value)
+			}
+		}
+
+		jsonData, err = MarshalData(item)
+		if err != nil {
+			return err
+		}
+
+		EditFile(args, jsonData)
+
+		bytes, err := Readfile(args["fileName"], writer)
+		if err != nil {
+			return err
+		}
+
+		err = WriteOut(bytes, writer)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
-func DeleteList() error {
-	return nil
+func IsIDExist(item []Item, args Arguments) bool {
+	var exist bool
+	for _, value := range item {
+		if value.Id == args["id"] {
+			exist = true
+		}
+	}
+	return exist
 }
 
-func MarshalData(args Arguments) ([]byte, error) {
-	js, err := json.Marshal(args)
+func EditFile(args Arguments, data []byte) error {
+	os.Remove(args["fileName"])
+
+	err := CreateFile(args["fileName"])
 	if err != nil {
-		return js, err
+		return err
 	}
-	return js, nil
+
+	_, err = Openfile(args["fileName"])
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(args["fileName"], data, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-// func UnmarshalData(js []byte) (Args, error) {
-// 	args := Args{}
-// 	err := json.Unmarshal(js, &args)
-// 	if err != nil {
-// 		return args, err
-// 	}
-// 	return args, err
-// }
+func CheckItemID(args Arguments, writer io.Writer) error {
+	var idToAdd string
+	var id2 int
 
-func Openfile(args Arguments) (err error) {
+	// jsonData, err := UnmarshalData([]byte(args["item"]))
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return err
+	// }
+
+	// log.Println(jsonData)
+
+	idToAdd = args["item"][7:8]
+
+	file, err := Openfile(args["fileName"])
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	jsonData, err := Readfile(file.Name(), writer)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	id2, err = strconv.Atoi(idToAdd)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	id3, err := strconv.Atoi(string(string(jsonData[8:9]))) // TODO
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	if id2 == id3 {
+		return errors.New(errSameId)
+	}
+
+	return nil
+}
+
+func Readfile(filename string, writer io.Writer) ([]byte, error) {
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes, err
+}
+
+func WriteOut(bytes []byte, writer io.Writer) error {
+	_, err := writer.Write(bytes)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func UnmarshalData(jsonData []byte) ([]Item, error) {
+	var item []Item
+	err := json.Unmarshal(jsonData, &item)
+	if err != nil {
+		return nil, err
+	}
+	return item, nil
+}
+
+func MarshalData(data interface{}) ([]byte, error) {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return jsonData, err
+	}
+	return jsonData, nil
+}
+
+func CheckIsFileOpen(args Arguments) (err error) {
 	switch args["fileName"] {
 	case "":
 		return errors.New(errorFlagFile)
 	default:
 		err = CreateFile(args["fileName"])
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
 
 func CreateFile(filename string) error {
-	_, err := os.OpenFile(filename, os.O_CREATE, 0644)
+	_, err := Openfile(filename)
 	if err != nil {
 		return err
 	}
@@ -97,22 +292,32 @@ func CreateFile(filename string) error {
 	return nil
 }
 
+func Openfile(filename string) (*os.File, error) {
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, err
+	}
+	// defer file.Close()
+	return file, nil
+}
+
 func Perform(args Arguments, writer io.Writer) (err error) {
+
 	if len(args["fileName"]) == 0 {
 		return errors.New(errorFlagFile)
 	}
 	switch args["operation"] {
 	case "add":
-		err = Add(args)
+		err = Add(args, writer)
 		return err
 	case "list":
-		err = List(args["fileName"], writer)
+		_, err = List(args, writer)
 		return err
 	case "findById":
-		err = FindByID()
+		err = FindByID(args, writer)
 		return err
 	case "remove":
-		err = DeleteList()
+		err = Remove(args, writer)
 		return err
 	case "":
 		return errors.New(OperationMissingError)
@@ -134,9 +339,12 @@ func parseArgs() Arguments {
 }
 
 func main() {
-	// str := `-operation "add" -item "{"id": "1", "email": "email@test.com", "age": 23}" -fileName "users.json"`
 	err := Perform(parseArgs(), os.Stdout)
 	if err != nil {
 		panic(err)
 	}
 }
+
+// args
+// str := `-operation "add" -item "{"id": "1", "email": "email@test.com", "age": 23}" -fileName "users.json"`
+// go run main.go -operation "add" -item "" -fileName "users.json"
